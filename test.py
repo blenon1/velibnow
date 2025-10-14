923a602cbc4e580447424d069e212a39b6d140d3
blenon1 <williamblenon@gmail.com>
2025-10-14 19:24:35 +0200

    ETL terminer v2

--- test.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from config import POSTGRES_URL
-
-print("🔍 Test de connexion à PostgreSQL...")
-engine = create_engine(POSTGRES_URL)
-with engine.connect() as conn:
-    print("✅ Connexion réussie !")
